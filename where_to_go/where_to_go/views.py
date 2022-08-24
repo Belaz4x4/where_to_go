b4337782c551c69f272fdde617bc2a5512bc2241
Villeman <95858575+Belaz4x4@users.noreply.github.com>
2022-08-24 20:44:18 +0300

    Add blank start page

--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
-
+from django.template import loader
 def start_page(reqest):
-    return HttpResponse('Здесь будет карта')
+    template = loader.get_template('index.html')
+    context = {}
+    rendered_page = template.render(context, reqest)
+    return HttpResponse(rendered_page)
